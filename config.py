44d01e8ea646f74f21c2f749d9244083d3fbc890
BharatChhimpa <bhartlal2013@gmail.com>
2025-03-31 18:21:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,29 +4,29 @@ class Config(object):
     
     BOT_TOKEN = "7903490948:AAFxqepZ4V0sG3hhr45Xvv55jQj2oJOg8vQ"
     
-    API_ID = 28094744
+    API_ID = 29683927
     
-    API_HASH = "a75af4285edc7747c57bb19147ca0b9b"
-    ADMIN_ID = [28094744]
+    API_HASH = "b2eb32ac1030edd8bc36c7b554ef6fc3"
+    ADMIN_ID = [5583858510]
     
-    DB_URL = "mongodb+srv://tmglcd:kI1UijMr2jJXyOXY@cluster0.unjzi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    DB_URL = "mongodb+srv://bkmathgpratap:WabfAUfmCDhntSb3@cluster0.uc74i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster00"
     
     DB_NAME = "MY_BOT_DB"
     
-    TXT_LOG = -1002416535442
+    TXT_LOG = -1002340192085
     
-    AUTH_LOG = -1002416535442
+    AUTH_LOG = -1002340192085
     
-    HIT_LOG = -1002416535442
+    HIT_LOG = -1002340192085
     
-    DRM_DUMP = -1002416535442
+    DRM_DUMP = -1002340192085
     # Main channel ki ID
-    CHANNEL = -1002416535442
+    CHANNEL = -1002340192085
     # Channel ka link
-    CH_URL = "https://t.me/nsmmsksmsmsmsmm"
+    CH_URL = "https://t.me/bkmaths1"
     
-    OWNER = "https://t.me/Nikhil_saini_khe"
+    OWNER = "https://t.me/bharat_chhimpa"
     
-    THUMB_URL = "https://tinypic.host/images/2025/02/07/IMG_20250207_224444_975.jpg" #Replace by with your Thumb URL
+    THUMB_URL = "https://tinypic.host/images/2023/06/21/_DSC1783-KRUPNYI-RAZMER.jpeg" #Replace by with your Thumb URL
    
     HOST = "https://api.masterapi.tech"
